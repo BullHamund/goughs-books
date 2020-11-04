9d7960029ca61371149cfa7776f778850dd06090
Bill Hammond <willmhammond@gmail.com>
2020-11-04 02:05:13 -0800

    added routes and started shop component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,21 +13,21 @@ class Directory extends Component {
           imageUrl:
             'https://cdn.pixabay.com/photo/2019/04/07/08/47/books-4109214__480.png',
           id: 1,
-          linkUrl: 'shop/fiction',
+          linkUrl: 'fiction',
         },
         {
           title: 'sci-fi',
           imageUrl:
             'https://cdn.pixabay.com/photo/2014/05/10/18/10/saturn-341379__480.jpg',
           id: 2,
-          linkUrl: 'shop/sci-fi',
+          linkUrl: 'sci-fi',
         },
         {
           title: 'FOREIGN LANGUAGE',
           imageUrl:
             'https://cdn.pixabay.com/photo/2016/07/28/20/34/book-1549589__480.jpg',
           id: 3,
-          linkUrl: 'shop/sneakers',
+          linkUrl: 'foreign-language',
         },
         {
           title: 'philosophy',
@@ -35,7 +35,7 @@ class Directory extends Component {
             'https://cdn.pixabay.com/photo/2015/11/19/21/11/knowledge-1052011_1280.jpg',
           size: 'large',
           id: 4,
-          linkUrl: 'shop/philosophy',
+          linkUrl: 'philosophy',
         },
         {
           title: 'theatre and film',
@@ -43,7 +43,7 @@ class Directory extends Component {
             'https://cdn.pixabay.com/photo/2015/07/02/10/07/audience-828584__480.jpg',
           size: 'large',
           id: 5,
-          linkUrl: 'shop/theatreAndFilm',
+          linkUrl: 'theatreAndFilm',
         },
       ],
     };
@@ -52,8 +52,8 @@ class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
